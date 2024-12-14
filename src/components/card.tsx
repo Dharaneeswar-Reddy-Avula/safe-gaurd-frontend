707b3996fe4902d8ea73d478965b79bfa95140f2
Dhanu <avuladharaneeswarreddy123@gmail.com>
2024-12-14 18:16:54 +0530

    newchnages

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-export const Card = (props) => {
+export const Card = (props: { image: string | undefined }) => {
   return (
-    <div className='card h-[200px] w-[200px] '>
+    <div className='card md:h-[200px] md:w-[200px] w-full'>
       <img src={props.image} className='w-full h-full'
       />
     </div>
